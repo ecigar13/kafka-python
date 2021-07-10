1460656ad282e33e0a8a4c2f47c6f86c38aab6c7
Keith, Nguyen <keith.nguyen@sabre.com>
2021-07-09 22:57:06 -0500

    Finished LineSplit and WordCount

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# kafka-python
+# kafka
+
+## What I learned
+- Very complex to start (Apache tutorial is wrong) - that's why there're companies like Confluent, Cloudurable offering manager service
+- I can make tweaks to the streams to filter, format messages before they're read.
+- After that, things get easier.
+
+## Pros
+- Easy to prototype (if I can get past the wrong tutorial)
+- Producers, consumers, transformers are modular
+
+## Cons
+- Tough to start from scratch. Java is verbose.
+- No native UI to manage.
+
+## Important commands
+```mvn compile exec:java -Dexec.mainClass="myapps.WordCount"```
--- streams.examples/src/main/java/myapps/LineSplit.java
+++ streams.examples/src/main/java/myapps/LineSplit.java
@@ -16,6 +16,8 @@
  */
 package myapps;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -26,6 +28,8 @@ import org.apache.kafka.streams.kstream.ValueMapper;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
+import static myapps.WordCount.getStreamsConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 
 /**
  * In this example, we implement a simple LineSplit program using the high-level Streams DSL
@@ -35,17 +39,39 @@ import java.util.concurrent.CountDownLatch;
  */
 public class LineSplit {
 
+    public static final String INPUT_TOPIC = "streams-plaintext-input";
+    public static final String OUTPUT_TOPIC = "streams-linesplit-output";
+
+    static Properties getStreamsConfig(final String[] args) throws IOException {
+        final Properties props = new Properties();
+        if (args != null && args.length > 0) {
+            try (final FileInputStream fis = new FileInputStream(args[0])) {
+                props.load(fis);
+            }
+            if (args.length > 1) {
+                System.out.println("Warning: Some command line arguments were ignored. This demo only accepts an optional configuration file.");
+            }
+        }
+        props.putIfAbsent(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount");
+        props.putIfAbsent(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.putIfAbsent(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        props.putIfAbsent(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.putIfAbsent(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
+        // Note: To re-run the demo, you need to use the offset reset tool:
+        // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
+        props.putIfAbsent(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return props;
+    }
+    
     public static void main(String[] args) throws Exception {
-        Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-linesplit");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-
+        final Properties props = getStreamsConfig(args);
         final StreamsBuilder builder = new StreamsBuilder();
-
+        
         builder.<String, String>stream("streams-plaintext-input")
-            .flatMapValues(value -> Arrays.asList(value.split("[0-9]")))
+            .filter((key, value) -> value != null)
+            .flatMapValues(value -> Arrays.asList(value.split(" ")))
             .to("streams-linesplit-output");
 
         final Topology topology = builder.build();
